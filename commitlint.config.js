b9606a89adf5ae94a45f159bd4214ebe1dcdd985
HaJunRyu <hjr4557@naver.com>
2024-05-12 15:16:39 +0900

    Env: commitlint config를 override한다

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,24 @@ module.exports = {
   rules: {
     "body-max-length": [0, "always", Infinity],
     "body-max-line-length": [0, "always", Infinity],
+    "type-case": [2, "always", "pascal-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "Deploy",
+        "Chore",
+        "Ci",
+        "Docs",
+        "Feat",
+        "Fix",
+        "Perf",
+        "Refactor",
+        "Revert",
+        "Style",
+        "Test",
+        "Env",
+      ],
+    ],
   },
 }
